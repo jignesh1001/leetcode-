c36679535dbfbda8a24f6d956b219f05af875f2b
Jignesh <jigneshgurav11@gmail.com>
2025-08-27 18:27:03 +0530

    Time: 31 ms (85.54%), Space: 72 MB (51.41%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.js
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.js
@@ -4,21 +4,23 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-     let distances = []
+    let distances = []
 
-    for(let i = 0; i < points.length ; i++){
-          let point = points[i]
-          let distance = (point[0]*point[0])+(point[1]*point[1])
-          distances.push([distance,point])
-    }
-   
-    distances.sort((a, b) => a[0] - b[0]);
+    for(let i = 0; i < points.length;i++){
+        let point = points[i]
 
-    
-    let result = [];
-    for (let i = 0; i < k; i++) {
-        result.push(distances[i][1]);
+        let distance = (point[0]*point[0])+(point[1]*point[1])
+
+        distances.push([distance,point])
     }
 
-    return result;
+    distances.sort((a,b) => a[0]-b[0])
+
+
+    let result = []
+
+    for(let i = 0; i < k;i++){
+        result.push(distances[i][1])
+    }
+    return result
 };
